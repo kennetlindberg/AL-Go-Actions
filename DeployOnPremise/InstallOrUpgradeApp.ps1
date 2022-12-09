8193975a544c3842c9be8a626343ae8d98b1d396
Kennet Lindberg <kennet@consilia.fi>
2022-12-09 15:19:49 +0200

    InstallOrUpgradeApp.ps1

--- DeployOnPremise/InstallOrUpgradeApp.ps1
+++ DeployOnPremise/InstallOrUpgradeApp.ps1
@@ -6,16 +6,25 @@ function InstallOrUpgradeApp {
     )
 
     $app = Get-NAVAppInfo -Path $path
+
+    Write-Host "Install Or Upgrade $($app.Name)"
+
     $published = Get-NAVAppInfo -ServerInstance $instance -Name $app.Name -Publisher $app.Publisher -TenantSpecificProperties -Tenant $tenant
- 
     $installed = $published | Where-Object -Property 'IsInstalled' -EQ -Value True | Select-Object -First 1
 
     Publish-NAVApp -ServerInstance $instance -Path $path    
     if ($installed) {        
-        Sync-NAVApp -ServerInstance $instance -Publisher $app.Publisher -Name $app.Name -Version $app.Version -Mode ForceSync -Force
-        Start-NAVAppDataUpgrade -ServerInstance $instance -Publisher $app.Publisher -Name $app.Name -Version $app.Version -Force
-        Unpublish-NAVApp -ServerInstance $instance -Publisher $installed.Publisher -Name $installed.Name -Version $installed.Version
+        if ($app.version -gt [System.Version]$installed.Version) {
+            Write-Host "Upgrading $($app.Name) from $($installed.Version) to $($app.Version)"
+
+            Sync-NAVApp -ServerInstance $instance -Publisher $app.Publisher -Name $app.Name -Version $app.Version -Mode ForceSync -Force
+            Start-NAVAppDataUpgrade -ServerInstance $instance -Publisher $app.Publisher -Name $app.Name -Version $app.Version -Force
+            Unpublish-NAVApp -ServerInstance $instance -Publisher $installed.Publisher -Name $installed.Name -Version $installed.Version
+        } else {
+            Write-Host "Newer version ($($installed.Version)) already installed"
+        }
     } else {
+        Write-Host "Installing $($app.Name) ($($app.Version))"
         Sync-NAVApp -ServerInstance $instance -Publisher $app.Publisher -Name $app.Name -Version $app.Version -Force
         Install-NAVApp -ServerInstance $instance -Publisher $app.Publisher -Name $app.Name -Version $app.Version -Force
     }
