a2d2e71cd48ccd830fb3a05165a13a09626193e0
Kennet Lindberg <31550641+kennetlindberg@users.noreply.github.com>
2022-12-20 14:28:57 +0200

    Update DeployOnPremise.ps1

--- DeployOnPremise/DeployOnPremise.ps1
+++ DeployOnPremise/DeployOnPremise.ps1
@@ -18,7 +18,7 @@ try {
 
     $instance = $settings.onPremServerInstance
     if ($instance -eq '') {
-        Write-Host $settingsJson
+        Write-Output $settingsJson
         throw "Setting onPremServerInstance needs to be specified".
     }
 
